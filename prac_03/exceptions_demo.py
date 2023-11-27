b924e61dfa645171118a4f89e984a8b5b29219fd
Cao Shaung Li <caoshaungli@my.jcu.edu.au>
2023-11-27 13:20:49 +0800

    Update exceptions demo program

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,11 +9,11 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
-    if denominator == 0:
+    while denominator == 0:
         print("Cannot divide by zero!")
-    else:
-        fraction = numerator / denominator
-        print(fraction)
+        denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
 # except ZeroDivisionError:
