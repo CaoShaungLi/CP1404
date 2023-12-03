2e9359c7a82ae784417c24a4b41d25593dc31c62
Cao Shaung Li <caoshaungli@my.jcu.edu.au>
2023-12-03 22:19:19 +0800

    Update title for programs

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,4 @@
+"""Intermediate Exercises"""
 COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff",
                   "Alizarin Crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
                   "Antique White": "#faebd7", "Antique White1": "#ffefdb", "Antique White2": "#eedfcc"}
--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,3 +1,4 @@
+"""Word Occurrences"""
 word_to_count = {}
 text = input("Text: ")
 words = text.split(" ")
