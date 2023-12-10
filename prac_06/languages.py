17bea3931efb1d394ec1c743f7f651d1cf45ed04
Cao Shaung Li <caoshaungli@my.jcu.edu.au>
2023-12-10 10:49:09 +0800

    Modify programming language client program

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,7 +1,10 @@
+"""Intermediate Exercise - Programming language client program"""
+
 from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
+    """Display programming languages that are dynamic"""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
