9b9b6b37588761ac166cca523dbaa0ef165e0aaa
Cao Shaung Li <caoshaungli@my.jcu.edu.au>
2023-12-10 10:49:26 +0800

    Modify guitar class program

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,3 +1,5 @@
+"""Guitar class program"""
+
 CURRENT_YEAR = 2023
 VINTAGE_MINIMUM_YEAR = 50
 
@@ -6,11 +8,13 @@ class Guitar:
     """Represent data of a guitar"""
 
     def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance"""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return string about guitar"""
         return f"{self.name} ({self.year} : ${self.cost})"
 
     def get_age(self):
