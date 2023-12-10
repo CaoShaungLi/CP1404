e6cb45585711b4e9af2a7e344f19307d282e22e6
Cao Shaung Li <caoshaungli@my.jcu.edu.au>
2023-12-10 10:50:04 +0800

    Modify guitars client program

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,4 +1,5 @@
 """Playing the Guitar - Guitars client program """
+
 from prac_06.guitar import Guitar
 
 
@@ -12,14 +13,22 @@ def main():
             year = int(input("Year: "))
             cost = float(input("Cost: $"))
             guitars.append(Guitar(name, year, cost))
+            print(f"{Guitar(name, year, cost)} is added to collection.")
         except ValueError:
             print("Invalid input")
         name = input("Name: ")
+
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
     print("...snip...")
-    print("These are my guitars:")
-    for i, guitar in enumerate(guitars, 1):
-        vintage_string = "(vintage)" if guitar.is_vintage() else ""
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = "(vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("There are no guitars yet.")
 
 
 main()
